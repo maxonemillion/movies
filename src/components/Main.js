a646cf528f6c6f3f9ad70949c37a22b5624dfc95
Maximilian Daniel <max.daniel@utah.edu>
2020-12-09 03:44:37 -0700

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 import React, { useState ***REMOVED*** from "react";
-import { Form, Button, Card, Container, Row, Col ***REMOVED*** from "react-bootstrap";
+import { Form, Button, Card, Container, Row, Col, InputGroup, FormControl ***REMOVED*** from "react-bootstrap";
 import API from "../utils/API";
 import "./Main.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Main = () => {
@@ -22,69 +23,83 @@ const Main = () => {
     e.preventDefault();
     loadTitles(searchTitle)
     setDisplay(!display)
-    
+
   ***REMOVED***;
 
   const handleKeypress = e => {
-  if (e.code === 13) {
-    handleSubmit();
-  ***REMOVED***
-***REMOVED***;
+    if (e.code === 13) {
+      handleSubmit();
+    ***REMOVED***
+  ***REMOVED***;
 
   return (
     <div>
       <Form.Group className="form">
         <Form.Control
+
           type="text"
           id="movie-input"
           placeholder="Find a movie"
           onChange={(event) => setSearchTitle(event.target.value)***REMOVED***
           onKeyPress={handleKeypress***REMOVED***
           value={searchTitle***REMOVED*** />
-        <Button
+          <div className="wrapper">
+        <Button className="btn"
           id="search-button"
-          variant="primary"
+          variant="light"
           type="submit"
           // onClick={() => loadTitles(searchTitle)***REMOVED***
           onClick={handleSubmit***REMOVED***
-        
-          
+
+
         >Search
             </Button>
+          </div>
         <br />
 
       </Form.Group>
-
-      <Card border="secondary" style={{ width: '18rem' ***REMOVED******REMOVED*** className="card">
-        <Card.Img border="primary" variant="top" className="image" src={movies.Poster***REMOVED*** />
-        <Card.Body>
-          <br></br>
-          <Card.Title className="title">{movies.Title***REMOVED***</Card.Title>
-          <Card.Text className="list">
-            <li className={display ? "display" : null***REMOVED***>
-              {movies.Plot***REMOVED***
-            <br></br>
-            <br></br>
-            Directed by {movies.Director***REMOVED***
-            <br></br>
-            Released in {movies.Year***REMOVED***
-            <br></br>
-            {movies.Genre***REMOVED***
-            <br></br>
-            Rated {movies.Rated***REMOVED***
-            <br></br>
-            IMDb: {movies.imdbRating***REMOVED***
-            </li>
-            <br></br>
-          </Card.Text>
-        </Card.Body>
-      </Card>
 
       {/* {Object.keys(movies).map(key => {
           if (!Array.isArray(movies[key])) {
             return <li>{key***REMOVED***: {movies[key]***REMOVED***</li>;
 ***REMOVED***
   ***REMOVED***)***REMOVED*** */***REMOVED***
+
+
+      <Container>
+        <Row>
+          <Col>
+            <Card style={{ width: '18rem', border: "none" ***REMOVED******REMOVED*** className="card1 ">
+              <img className={display ? "display" : null***REMOVED*** id="image" src={movies.Poster***REMOVED*** />
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '18rem', border: "none" ***REMOVED******REMOVED*** className="card">
+              <Card.Body>
+                <br></br>
+                <Card.Title className="title">{movies.Title***REMOVED***</Card.Title>
+                <Card.Text className="list">
+                  <li className={display ? "display" : null***REMOVED***>
+                    {movies.Plot***REMOVED***
+                    <br></br>
+                    <br></br>
+            Directed by {movies.Director***REMOVED***
+                    <br></br>
+            Released in {movies.Year***REMOVED***
+                    <br></br>
+                    {movies.Genre***REMOVED***
+                    <br></br>
+            Rated {movies.Rated***REMOVED***
+                    <br></br>
+            IMDb: {movies.imdbRating***REMOVED***
+                  </li>
+                  <br></br>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
 
     </div >
   )
