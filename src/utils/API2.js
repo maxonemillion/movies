bfda6e469da8c9cbc1a4fe6340892cbaef79331c
Maximilian Daniel <max.daniel@utah.edu>
2020-12-14 19:29:18 -0700

    updates

--- src/utils/API2.js
+++ src/utils/API2.js
@@ -5,9 +5,10 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
           params: {source_id: id, source: 'imdb', country: 'us'***REMOVED***,
 ***REMOVED***
-***REMOVED***
+            'x-rapidapi-key': '***REMOVED***',
 ***REMOVED***
         ***REMOVED***
       ***REMOVED***;
